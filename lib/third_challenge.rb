65d75748320624a825f631125ef061cfef578ae7
David Ingram <david@ingramaviation.com>
2019-07-19 03:30:58 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,18 +11,26 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
+        :name => "Lord Capulet",
+        :age => "50",
         #your key/value pairs here
         },
       :matriarch => {
+        :name => "Lady Capulet",
+        :age => "51"
         #your key/value pairs here
         },
       :heroine => {
+        :name => ""
         #your key/value pairs here
         },
       :heroine_friends => []
