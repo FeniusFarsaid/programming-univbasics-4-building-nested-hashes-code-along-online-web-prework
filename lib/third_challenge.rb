14fa59cea7d4d7bc294fe178c2f25bba52c4fae4
David Ingram <david@ingramaviation.com>
2019-07-19 03:31:44 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -30,7 +30,9 @@ def third_challenge
         #your key/value pairs here
         },
       :heroine => {
-        :name => ""
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
